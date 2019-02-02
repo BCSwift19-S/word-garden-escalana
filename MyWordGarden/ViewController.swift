c3e3ee2e008eee555eeecc6644d8413da5f86c52
Alessandra Escalante <escalana@bc.edu>
2019-02-02 16:08:25 -0500

    3.2 completed and kept print statements

--- MyWordGarden/ViewController.swift
+++ MyWordGarden/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userGuessLabel: UILabel!
+    
+    @IBOutlet weak var GuessLetterField: UITextField!
+    @IBOutlet weak var GuessLetterButton: UIButton!
+    
+    @IBOutlet weak var GuessCountLabel: UILabel!
+    @IBOutlet weak var PlayAgainButton: UIButton!
+    
+    @IBOutlet weak var FlowerImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print("In viewDidLoad, is GuessLetterField the first responder", GuessLetterField.isFirstResponder)
+    }
+    
+    func updateUIAfterGuess(){
+        GuessLetterField.resignFirstResponder()
+        GuessLetterField.text = " "
+    }
+    
+    @IBAction func GuessLetterFieldChange(_ sender: UITextField) {
+        print("The GuessedLetterField changed!" )
     }
 
+    @IBAction func doneKeyPressed(_ sender: UITextField) {
+         print("In doneKeyPressed, is GuessLetterField the first responder", GuessLetterField.isFirstResponder)
+        updateUIAfterGuess()
+         print("In doneKeyPressed, is GuessLetterField the first responder", GuessLetterField.isFirstResponder)
+    }
+    
+    @IBAction func GuessLetterButtonPressed(_ sender: UIButton) {
+         print("In GuessLetterButtonPressed, is GuessLetterField the first responder", GuessLetterField.isFirstResponder)
+        updateUIAfterGuess()
+        print("In GuessLetterButtonPressed, is GuessLetterField the first responder", GuessLetterField.isFirstResponder)
+        
+    }
+    
+    @IBAction func PlayAgainButtonPressed(_ sender: UIButton) {
+    }
+    
+    
+    
 
 }
 
